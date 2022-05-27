62c955f2ccee2e175fe017579ff5f3911c9a761f
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-05-27 16:01:49 +0200

    feat: ad geoip query

---
+++ src/main/scala/GeoQuery.scala
@@ -0,0 +1,19 @@
+package it.unibo.bd
+
+import Main.RichRDD
+
+import org.apache.spark.{ SparkConf, SparkContext }
+
+object GeoQuery extends App {
+  if (args.length < 1) throw new IllegalArgumentException("Dataset path is required")
+
+  val sc = new SparkContext(new SparkConf().setAppName("GeoQuery"))
+  println(s"Application started at http://localhost:20888/proxy/${sc.applicationId}/\n")
+
+  val datasetPath = args(1)
+  val rawDataset = sc.textFile(datasetPath)
+
+  val dataset = rawDataset.skip(1).map(_.split(",").toSeq)
+  val header = rawDataset.first().split(",").tail.toSeq
+
+}
