f3e6f9cc49294efa6c2eb56b06bd5257469f3ca5
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-06-21 11:28:21 +0200

    feat: add images

--- src/main/scala/queries/NetProtocol.scala
+++ src/main/scala/queries/NetProtocol.scala
@@ -1,13 +1,52 @@
 package it.unibo.bd
 package queries
 
+import com.cibo.evilplot.plot.PieChart
+import com.cibo.evilplot.plot.aesthetics.DefaultTheme.defaultTheme
+import it.unibo.bd.utils.Record
 import org.apache.spark.{ SparkConf, SparkContext }
+
+import java.io.File
 
 object NetProtocol {
 
   def main(args: Array[String]): Unit = {
     val sc = new SparkContext(new SparkConf().setAppName("NetProtocol"))
     println(s"Application started at http://localhost:20888/proxy/${sc.applicationId}/\n")
+
+    val pathTCPDataset = s"${args(0)}/DDoS_TCP.csv"
+    val pathUDPDataset = s"${args(0)}/DDoS_UDP.csv"
+    val pathHTTPDataset = s"${args(0)}/DDoS_HTTP.csv"
+    val dataset = sc.textFile(s"$pathTCPDataset,$pathUDPDataset,$pathHTTPDataset")
+
+    val recordDataset =
+      dataset
+        .map(_.replace("\"", ""))
+        .map(_.split(";"))
+        .map(Record(_))
+        .filter(_.isDefined)
+        .map(_.get)
+        .cache()
+
+    val ddosByProtocol = recordDataset.filter(_.isDDoS).map(_.protocol).countByValue()
+
+    println(ddosByProtocol)
+
+    val tcpPercentage = ddosByProtocol("tcp") / ddosByProtocol.values.sum.toDouble * 100
+    val udpPercentage = ddosByProtocol("udp") / ddosByProtocol.values.sum.toDouble * 100
+
+    val file = new File("/home/nicolas/Documents/uni/lm/bd/bd-ddos-analysis/protocol-chart.png")
+
+    if (!file.createNewFile()) throw new IllegalArgumentException("No file created...")
+    PieChart(
+      Seq(
+        f"TCP: ($tcpPercentage%2.2f%%)" -> ddosByProtocol("tcp"),
+        f"UDP: ($udpPercentage%2.2f%%)" -> ddosByProtocol("udp"),
+      ),
+    )
+      .rightLegend(labels = Some(Seq("TCP packets", "UDP packets")))
+      .render()
+      .write(file)
 
     /*
     val packets =
